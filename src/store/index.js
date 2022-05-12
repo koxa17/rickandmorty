49da49dce943296a3717a0e0cf573c9861933089
koxa17 <n.matusevich@clicktoid.ru>
2022-05-12 17:12:53 +0500

    init project

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,35 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import {CHARACTERS_BY_PAGE} from "../api/routes";
+import instance from "../api/api";
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    characters: {},
+    pages: 0
   },
   mutations: {
+    setCharacters: (state, {page, characters}) => {
+      state.characters[page] = characters
+    },
+    setPages: (state, pages) => {
+      state.pages = pages
+    }
   },
   actions: {
+    getApiCharacter({commit}, page) {
+      return instance.get(CHARACTERS_BY_PAGE(page)).then(res => {
+        const {info, results} = res.data
+        console.log(res.data)
+        commit('setCharacters', {page, characters: results})
+        commit('setPages', info.pages)
+      }).catch(err => {
+        console.log(err)
+      })
+    }
   },
-  modules: {
-  }
+  getters: {}
+
 })
---
+++ src/views/RickAndMorty.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+     <h1>Rick And Morty</h1>
+    <RickAndMortyCharacters></RickAndMortyCharacters>
+  </div>
+</template>
+
+<script>
+
+import RickAndMortyCharacters from "./RickAndMortyCharacters";
+export default {
+  name: 'RickAndMorty',
+  components: {
+    RickAndMortyCharacters
+  },
+  created() {
+    this.$store.dispatch('getApiCharacter', 1)
+  }
+}
+</script>
